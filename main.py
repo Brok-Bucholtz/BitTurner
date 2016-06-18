cde707061c02089d37d839d27d187420f07ee188
Brok Bucholtz <brok.bucholtz@gmail.com>
2016-06-18 16:32:57 -0700

    Add button to switch screens

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from kivy.app import App
+from kivy.uix.button import Button
 from kivy.uix.label import Label
 from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.pagelayout import PageLayout
 from kivy.uix.codeinput import CodeInput
 from kivy.config import Config
+from kivy.uix.screenmanager import ScreenManager, Screen
 
 from pygments.styles.borland import BorlandStyle
 from pygments.lexers.python import Python3Lexer
@@ -13,22 +14,37 @@ Config.set('graphics', 'width', '1100')
 Config.set('graphics', 'height', '800')
 
 
+def _create_screen(content, name):
+    def switch_screen(screen_manager): screen_manager.current = screen_manager.next()
+
+    screen = Screen(name=name)
+    layout = BoxLayout(orientation='vertical')
+    swap_button = Button(text='Swap Screen')
+    swap_button.bind(on_release=lambda _: switch_screen(screen.manager))
+    layout.add_widget(swap_button)
+    layout.add_widget(content)
+    screen.add_widget(layout)
+
+    return screen
+
+
 class TestApp(App):
     def build(self):
         code = "import datetime\nprint datetime.now().strftime('%Y-%m-%d %H:%M:%S')"
+        parent_layout = ScreenManager()
 
-        parent_layout = PageLayout()
-        page_1 = BoxLayout(orientation='horizontal')
-        parent_layout.add_widget(page_1)
-        page_2 = BoxLayout(orientation='horizontal')
-        parent_layout.add_widget(page_2)
-
+        editor_layout = BoxLayout(orientation='horizontal')
         layout1 = BoxLayout(orientation='vertical')
         layout1.add_widget(Label(text='Input'))
         layout1.add_widget(Label(text='Output'))
-        page_1.add_widget(layout1)
+        editor_layout.add_widget(layout1)
+        editor_layout.add_widget(CodeInput(text=code, lexer=Python3Lexer(), style=BorlandStyle))
 
-        page_1.add_widget(CodeInput(text=code, lexer=Python3Lexer(), style=BorlandStyle))\
+        job_layout = BoxLayout(orientation='horizontal')
+        job_layout.add_widget(Label(text='TODO'))
+
+        parent_layout.add_widget(_create_screen(editor_layout, 'editor'))
+        parent_layout.add_widget(_create_screen(job_layout, 'Jobs'))
 
         return parent_layout
 
